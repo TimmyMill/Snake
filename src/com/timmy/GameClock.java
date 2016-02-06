4bac1562d4118efb0c966e48a54da672005beb5c
timmy <timmymill87@gmail.com>
2016-02-06 12:28:01 -0600

    Tidied up GameClock's code.

--- src/com/timmy/GameClock.java
+++ src/com/timmy/GameClock.java
@@ -2,32 +2,38 @@ package com.timmy;
 
 import java.util.TimerTask;
 
-public class GameClock extends TimerTask {
-
+public class GameClock extends TimerTask
+{
 	Snake snake;
 	Kibble kibble;
 	Score score;
 	DrawSnakeGamePanel gamePanel;
 		
-	public GameClock(Snake snake, Kibble kibble, Score score, DrawSnakeGamePanel gamePanel){
+	public GameClock(Snake snake, Kibble kibble, Score score, DrawSnakeGamePanel gamePanel)
+	{
 		this.snake = snake;
 		this.kibble = kibble;
 		this.score = score;
 		this.gamePanel = gamePanel;
 	}
-	
+
+
 	@Override
-	public void run() {
+	public void run()
+	{
 		// This method will be called every clock tick
-						
 		int stage = SnakeGame.getGameStage();
 
-		switch (stage) {
-			case SnakeGame.BEFORE_GAME: {
+		switch (stage)
+		{
+			case SnakeGame.BEFORE_GAME:
+			{
 				//don't do anything, waiting for user to press a key to start
 				break;
 			}
-			case SnakeGame.DURING_GAME: {
+
+			case SnakeGame.DURING_GAME:
+			{
 				snake.moveSnake();
 				if (snake.didEatKibble(kibble)) {
 					//tell kibble to update
@@ -36,22 +42,26 @@ public class GameClock extends TimerTask {
 				}
 				break;
 			}
-			case SnakeGame.GAME_OVER: {
+
+			case SnakeGame.GAME_OVER:
+			{
 				this.cancel();
-				break;
-			}
-			case SnakeGame.GAME_WON: {
-				this.cancel();
-				break;
-			}
-			default: {
-				System.out.println("Not in a game stage");
 				break;
 			}
 
+			case SnakeGame.GAME_WON:
+			{
+				this.cancel();
+				break;
+			}
+
+			default:
+			{
+				System.out.println("Not in a game stage");
+				break;
+			}
 		}
-				
+
 		gamePanel.repaint();		//In every circumstance, must update screen
-		
 	}
 }
